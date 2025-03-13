c6a10326182910b9a8e35a215d0ed414dc19ca2d
MADHUSUDANA M <119025777+Mad705@users.noreply.github.com>
2025-03-13 16:43:58 +0530

    Update hello.cpp FUN

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "changed the network , pes1ug22cs320 \n";
+  cout << "CC LAB FUN FUN, pes1ug22cs320 \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
