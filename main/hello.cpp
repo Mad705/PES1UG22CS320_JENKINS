7e74a98c6a9dc9dc3e81aadbf2ee5464035f9a04
MADHUSUDANA M <119025777+Mad705@users.noreply.github.com>
2025-03-13 16:30:14 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "this is the change i made helo sir , pes1ug22cs320 \n";
+  cout << "this is the change i made iphone 13, pes1ug22cs320 \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
