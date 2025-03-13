600986e5a054ffdb5c6017e666e09e071984ecbb
MADHUSUDANA M <119025777+Mad705@users.noreply.github.com>
2025-03-13 16:35:19 +0530

    Update hello.cpp changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "work this time, pes1ug22cs320 \n";
+  cout << "changed the network , pes1ug22cs320 \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
